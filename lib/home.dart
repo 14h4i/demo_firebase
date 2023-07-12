028b11dccba38222eb66300cd409e4047fe86eed
14h4i <pgkhai.dev@gmail.com>
2023-07-12 22:25:55 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demo_firebase/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +16,14 @@ class _HomePageState extends State<HomePage> {
   User? user;
 
   String? a;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Map<String, dynamic> hocsinh = {};
+
+  List<Map<String, dynamic>> listHocsinh = [];
+
+  final controller = TextEditingController();
 
   @override
   void initState() {
@@ -36,7 +47,120 @@ class _HomePageState extends State<HomePage> {
             },
             icon: const Icon(Icons.logout))
       ]),
-      body: Center(child: Text('Welcome : $emailUser')),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              child: const Icon(Icons.close),
+              onPressed: () async {
+                hocsinh = {};
+                listHocsinh = [];
+
+                setState(() {});
+              }),
+          FloatingActionButton(
+              child: const Icon(Icons.get_app),
+              onPressed: () async {
+                final response =
+                    await firestore.collection('data').doc('doc-id-1').get();
+
+                final data = response.data();
+
+                if (data != null) {
+                  hocsinh = data;
+
+                  // final name = hocsinh['nameStudent'];
+
+                  // print('Ten hoc sinh la: $name');
+
+                  setState(() {});
+                }
+              }),
+          FloatingActionButton(
+              child: const Icon(Icons.download_for_offline_outlined),
+              onPressed: () async {
+                final response = await firestore.collection('data').get();
+
+                final data = response.docs;
+
+                listHocsinh = [];
+
+                for (var i = 0; i < data.length; i++) {
+                  final hocsinh = data[i].data();
+
+                  listHocsinh.add(hocsinh);
+                }
+
+                setState(() {});
+
+                print(listHocsinh);
+              }),
+        ],
+      ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('Welcome : $emailUser'),
+            const SizedBox(
+              height: 50,
+            ),
+            Text("Ten hoc sinh: ${hocsinh['nameStudent']}"),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Danh sach hoc sinh:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: listHocsinh.length,
+                itemBuilder: ((context, index) {
+                  final hocsinh = listHocsinh[index];
+
+                  final name = hocsinh['nameStudent'];
+
+                  return ListTile(
+                    title: Text(name),
+                  );
+                })),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: controller,
+                decoration: const InputDecoration(border: OutlineInputBorder()),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  firestore.collection('data').add({
+                    'nameStudent': controller.text,
+                    'age': 19,
+                  });
+                  // firestore.collection('data').doc('doc-id-3').set({
+                  //   'nameStudent': controller.text,
+                  //   'age': 19,
+                  // });
+                },
+                child: const Text('Add')),
+            ElevatedButton(
+                onPressed: () {
+                  firestore.collection('data').doc('doc-id-2').update({
+                    'nameStudent': controller.text,
+                  });
+                },
+                child: const Text('Update')),
+          ],
+        ),
+      )),
     );
   }
 }
